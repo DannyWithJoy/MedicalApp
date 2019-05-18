f6a6dae1308d0628648cd679c7ed04c7eda4d384
许冬 <danny@DannyXu-MacBook-Pro.local>
2019-05-18 13:47:03 +0800

    Init commit.

---
+++ Medical/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Medical
+//
+//  Created by 许冬 on 2019/5/18.
+//  Copyright © 2019 Danny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Medical/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Medical
+//
+//  Created by 许冬 on 2019/5/18.
+//  Copyright © 2019 Danny. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
